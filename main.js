ad6c7188892ce505a5e94a7fccdd0d539212cb78
Roshanbr2009106 <77448893+Roshanbr2009106@users.noreply.github.com>
2021-09-15 19:17:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function bedroom(){
+    window.location = "bedroom.html";
+}
+function tv_ac(){
+    window.location = "tvac.html";
+}
+function desk(){
+    window.location = "desk.html";
+}
+function bottles(){
+      window.location = "bottle.html";
+}
+function fruitbasket(){
+    window.location = "fruit.html";
+}
---
+++ tvac.js
@@ -0,0 +1,52 @@
+function back(){
+    window.location = "index.html";
+}
+img = "";
+status = "";
+objects = [];
+
+function preload(){
+    img = loadImage("tvac.jpeg");
+
+}
+function setup(){
+    canvas = createCanvas(380,380);
+    canvas.center();
+    
+    Objectdetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+
+}
+function modelLoaded(){
+    console.log("model is loaded");
+    status = true;
+    Objectdetector.detect(img,gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+    console.error(error);
+    }
+    console.log(results);
+    objects = results;
+
+}
+
+function draw(){
+    image(img,0,0,380,380);
+    if(status != ""){
+        for(i = 0; i <= objects.length(); i++){
+            r = random(255);
+            g = random(255);
+            b = random(255);
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            fill(r,g,b);
+            percentage = floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percentage+"%",objects[i].x + 15,objects[i].y + 15);
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+
+        }
+    }
+}
